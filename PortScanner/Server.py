74191a62a56c2400a00ae583d140113badf6484f
张睿 <rzhang3@xiaohongshu.com>
2018-11-07 20:45:21 +0800

    init

--- PortScanner/Server.py
+++ PortScanner/Server.py
@@ -9,7 +9,7 @@ def handle_client(client_socket):
     处理客户端请求
     """
     request_data = client_socket.recv(1024)
-    print("request data:", request_data)
+    print request_data
     # 构造响应数据
     response_start_line = "HTTP/1.1 200 OK\r\n"
     response_headers = "Server: My server\r\n"
